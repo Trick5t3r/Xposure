eab6cabc94e3b69f6e67f8fb70425b6a75bef814
Paul Le Van Kiem <paul.levankiem@gmail.com>
2025-01-12 22:43:00 +0100

    Massive changes

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -12,8 +12,8 @@ function NavBar() {
     return <div className="nav-bar">
         <span className="nav-bar-logo">{CompanyTitle}</span>
         <div className="nav-bar-p">
-            {navBarcontent.map((item) => {
-                return <p>{item.name}</p>
+            {navBarcontent.map((item, index) => {
+                return <p key={index}>{item.name}</p>
             })}
         </div>
         {/* <form><div className="nav-bar-input-wrapper"><span className="nav-bar-search-logo">&#128269;</span><input className="nav-bar-search" type="text" placeholder="Search" /></div></form> */}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,14 +9,16 @@ function HomePage() {
         {title: "Key Themes", description: "Extract key themes from your press coverage to understand what topics are being discussed."},
         {title: "Comprehensive Reports", description: "Generate comprehensive reports that summarize the sentiment and key themes of your press coverage."}
     ]
-    const teamCards = [
-        {name: "John Doe", role: "CEO", description: "John is the CEO of our company and has over 10 years of experience in the industry."},
-        {name: "Jane Doe", role: "CTO", description: "Jane is the CTO of our company and is an expert in artificial intelligence and machine learning."},
-        {name: "John Doe", role: "CEO", description: "John is the CEO of our company and has over 10 years of experience in the industry."},
-        {name: "Jane Doe", role: "CTO", description: "Jane is the CTO of our company and is an expert in artificial intelligence and machine learning."},
-        {name: "John Doe", role: "CEO", description: "John is the CEO of our company and has over 10 years of experience in the industry."},
-        {name: "Jane Doe", role: "CTO", description: "Jane is the CTO of our company and is an expert in artificial intelligence and machine learning."}
-    ]
+    const teamCards1 = [
+        {name: "Théo Le Pendeven", role: "CEO", description: "Théo is the king of the goblins. He designed", ImageUrl: "/imgs/theo.jpg"},
+        {name: "Paul Le Van Kiem", role: "CTO", description: "Jane is the CTO of our company and is an expert in artificial intelligence and machine learning.", ImageUrl: "/imgs/lvk.jpg"},
+        {name: "Yessin Moakher", role: "CEO", description: "John is the CEO of our company and has over 10 years of experience in the industry.", ImageUrl: "/imgs/yessin.jpg"},
+    ];
+    const teamCards2 = [
+        {name: "David Kerriou", role: "CTO", description: "Jane is the CTO of our company and is an expert in artificial intelligence and machine learning.", ImageUrl: "/imgs/david.jpg"},
+        {name: "Colas Lepoutre", role: "CEO", description: "John is the CEO of our company and has over 10 years of experience in the industry.", ImageUrl: "/imgs/colas.jpg"},
+        {name: "Baptiste Barthe-Gold", role: "CTO", description: "Jane is the CTO of our company and is an expert in artificial intelligence and machine learning.", ImageUrl: "/imgs/barthe.PNG"}
+    ];
     return <>
             <div className="home-top-page">
                 <NavBar />
@@ -33,8 +35,8 @@ function HomePage() {
             </div>
             <div className="home-middle-page">
                 <div className="home-middle-page-wrapper">
-                    {featuresCards.map((card) => {
-                        return <div className="home-middle-page-card">
+                    {featuresCards.map((card, index) => {
+                        return <div key={index} className="home-middle-page-card">
                             <div className="home-middle-page-card-container">
                                 <h3>{card.title}</h3>
                                 <p>{card.description}</p>
@@ -42,19 +44,32 @@ function HomePage() {
                         </div>
                     })}
                 </div>
+                <div className="separator"></div>
                 <div className="home-about-us">
                     <h3>About Us</h3>
                     <div className="home-about-us-wrapper">
-                        {teamCards.map((card) => {
-                            return <div className="home-about-us-card">
+                        {teamCards1.map((card, index) => {
+                            return <div key={index} className="home-about-us-card">
                                 <div className="home-about-us-card-container">
+                                    <img src={card.ImageUrl} alt={card.name} className="home-photo" />
                                     <h4>{card.name}</h4>
                                     <p>{card.role}</p>
                                     <p>{card.description}</p>
                                 </div>
                             </div>
                         })}
-
+                    </div>
+                    <div className="home-about-us-wrapper">
+                        {teamCards2.map((card, index) => {
+                            return <div key={index} className="home-about-us-card">
+                                <div className="home-about-us-card-container">
+                                    <img src={card.ImageUrl} alt={card.name} className="home-photo" />
+                                    <h4>{card.name}</h4>
+                                    <p>{card.role}</p>
+                                    <p>{card.description}</p>
+                                </div>
+                            </div>
+                        })}
                     </div>
                 </div>
             </div>
