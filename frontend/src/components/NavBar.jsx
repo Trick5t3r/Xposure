c331d1f0a0f27e73ade07735e0dd008077ba15fe
Paul Le Van Kiem <paul.levankiem@gmail.com>
2025-01-13 18:29:02 +0100

    oui

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,25 +1,39 @@
 import "../styles/NavBar.css";
+import {useNavigate} from "react-router-dom";
 
 function NavBar() {
     const CompanyTitle = "Xposure";
+    const navigate = useNavigate();
+    const handleScroll = () => {
+        const element = document.getElementById('target');
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
 
     const navBarcontent = [
-        {name: "Features"},
+        {name: "Dashboard", route: "/dashboard"},
         {name: "GitHub"},
         {name: "About us"}
     ];
 
     return <div className="nav-bar">
-        <span className="nav-bar-logo">{CompanyTitle}</span>
+        <span className="nav-bar-logo" onClick={() => navigate("/")}>{CompanyTitle}</span>
         <div className="nav-bar-p">
             {navBarcontent.map((item, index) => {
-                return <p key={index}>{item.name}</p>
+                if (item.name === "Dashboard") {
+                    return <p key={index} onClick={() => navigate(item.route)}>{item.name}</p>
+                } else if (item.name === "About us") {
+                    return <p key={index} onClick={handleScroll}>{item.name}</p>
+                } else {
+                    return <p key={index}><a href="https://github.com/Trick5t3r/Xposure" target="_blank" rel="noopener noreferrer">{item.name}</a></p>
+                }
             })}
         </div>
         {/* <form><div className="nav-bar-input-wrapper"><span className="nav-bar-search-logo">&#128269;</span><input className="nav-bar-search" type="text" placeholder="Search" /></div></form> */}
         <div className="nav-bar-button-container">
-            <button className="nav-bar-secondary-button">Login</button>
-            <button className="nav-bar-primary-button">Get started</button>
+            <button className="nav-bar-secondary-button" onClick={() => navigate("/login")}>Login</button>
+            {/* <button className="nav-bar-primary-button">Get started</button> */}
         </div>
     </div>
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,15 +10,18 @@ function HomePage() {
         {title: "Comprehensive Reports", description: "Generate comprehensive reports that summarize the sentiment and key themes of your press coverage."}
     ]
     const teamCards1 = [
-        {name: "Théo Le Pendeven", role: "CEO", description: "Théo is the king of the goblins. He designed", ImageUrl: "/imgs/theo.jpg"},
-        {name: "Paul Le Van Kiem", role: "CTO", description: "Jane is the CTO of our company and is an expert in artificial intelligence and machine learning.", ImageUrl: "/imgs/lvk.jpg"},
-        {name: "Yessin Moakher", role: "CEO", description: "John is the CEO of our company and has over 10 years of experience in the industry.", ImageUrl: "/imgs/yessin.jpg"},
+        {name: "Théo Le Pendeven", role: "King of Goblins", description: "Théo is the king of the goblins. He likes earning money while programming. Thank you Théo !", ImageUrl: "/imgs/theo.jpg", linkedin: "https://www.linkedin.com/in/th%C3%A9o-le-pendeven-038067247?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2FhDCfsJoSCejHCuet14owg%3D%3D"},
+        {name: "Paul Le Van Kiem", role: "Ja", description: "Jane is the CTO of our company and is an expert in artificial intelligence and machine learning.", ImageUrl: "/imgs/lvk.jpg", linkedin: "https://www.linkedin.com/in/paul-le-van-kiem-000246256?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BcQT0dANBSgG3xvsvu81uyg%3D%3D"},
+        {name: "Yessin Moakher", role: "La spéciale", description: "John is the CEO of our company and has over 10 years of experience in the industry.", ImageUrl: "/imgs/yessin.jpg", linkedin: "https://www.linkedin.com/in/yessin-moakher-600a54246?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B3YMB7Hx1QwyRrdcg51M1yA%3D%3D"},
     ];
     const teamCards2 = [
-        {name: "David Kerriou", role: "CTO", description: "Jane is the CTO of our company and is an expert in artificial intelligence and machine learning.", ImageUrl: "/imgs/david.jpg"},
-        {name: "Colas Lepoutre", role: "CEO", description: "John is the CEO of our company and has over 10 years of experience in the industry.", ImageUrl: "/imgs/colas.jpg"},
-        {name: "Baptiste Barthe-Gold", role: "CTO", description: "Jane is the CTO of our company and is an expert in artificial intelligence and machine learning.", ImageUrl: "/imgs/barthe.PNG"}
+        {name: "David Kerriou", role: "Golémisateur", description: "Jane is the CTO of our company and is an expert in artificial intelligence and machine learning.", ImageUrl: "/imgs/david.jpg", linkedin: "https://www.linkedin.com/in/david-kerriou-75a939293?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BSyuXDFhJTaWSNHwkD%2Bxn1w%3D%3D"},
+        {name: "Colas Lepoutre", role: "Jawline Man", description: "John is the CEO of our company and has over 10 years of experience in the industry.", ImageUrl: "/imgs/colas.jpg", linkedin: "https://www.linkedin.com/in/colas-lepoutre/overlay/contact-info/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BfbtZuTv8TpGyY8ohb9aJ0Q%3D%3D"},
+        {name: "Baptiste Barthe-Gold", role: "Stanky Legg", description: "Jane is the CTO of our company and is an expert in artificial intelligence and machine learning.", ImageUrl: "/imgs/barthe.PNG", linkedin: "https://www.linkedin.com/in/baptiste-barthe-gold?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BqT2fg3a1TaaCwE93upDDEw%3D%3D"}
     ];
+    const handleRedirect = (link) => {
+        window.open(link, "_blank");
+    };
     return <>
             <div className="home-top-page">
                 <NavBar />
@@ -44,16 +47,16 @@ function HomePage() {
                         </div>
                     })}
                 </div>
-                <div className="separator"></div>
+                <div id="target" className="separator"></div>
                 <div className="home-about-us">
                     <h3>About Us</h3>
                     <div className="home-about-us-wrapper">
                         {teamCards1.map((card, index) => {
                             return <div key={index} className="home-about-us-card">
-                                <div className="home-about-us-card-container">
+                                <div className="home-about-us-card-container" onClick={() => handleRedirect(card.linkedin)}>
                                     <img src={card.ImageUrl} alt={card.name} className="home-photo" />
                                     <h4>{card.name}</h4>
-                                    <p>{card.role}</p>
+                                    <p className='home-role'>{card.role}</p>
                                     <p>{card.description}</p>
                                 </div>
                             </div>
@@ -62,10 +65,10 @@ function HomePage() {
                     <div className="home-about-us-wrapper">
                         {teamCards2.map((card, index) => {
                             return <div key={index} className="home-about-us-card">
-                                <div className="home-about-us-card-container">
+                                <div className="home-about-us-card-container" onClick={() => handleRedirect(card.linkedin)}>
                                     <img src={card.ImageUrl} alt={card.name} className="home-photo" />
                                     <h4>{card.name}</h4>
-                                    <p>{card.role}</p>
+                                    <p className='home-role'>{card.role}</p>
                                     <p>{card.description}</p>
                                 </div>
                             </div>
